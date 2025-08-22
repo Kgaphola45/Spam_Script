5e9067b287b676fcbe1718621d6d7eed0ffd952c
Kgaphola45 <It45ma@protonmail.ch>
2025-08-22 10:07:15 +0200

    Implement message sending functionality using pyautogui

--- main.py
+++ main.py
@@ -7,3 +7,8 @@ count = int(input("How many times do you want to send it? "))
 
 print("You have 5 seconds to click on the input field where messages will be sent...")
 time.sleep(5)
+
+for i in range(count):
+    pyautogui.typewrite(message)
+    pyautogui.press("enter")
+    time.sleep(0.3)
