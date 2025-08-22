b93318e41ada655227644cea7f3746b649684f2c
Kgaphola45 <It45ma@protonmail.ch>
2025-08-22 10:06:55 +0200

    Add user input prompts for message and count

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 import pyautogui
 import time
+
+
+message = input("Enter the message you want to send: ")
+count = int(input("How many times do you want to send it? "))
+
+print("You have 5 seconds to click on the input field where messages will be sent...")
+time.sleep(5)
